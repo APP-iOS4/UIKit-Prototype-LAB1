f55141fec6d2e58c66402e19f7a3ed7bdda952c7
Sanghyeon Park <ios.humhae@gmail.com>
2024-01-17 20:42:46 +0900

    feat: 새 브랜치 생성

---
+++ Kiosk/Kiosk/Controller/ChoiceBreadViewController.swift
@@ -0,0 +1,164 @@
+//
+//  ChoiceBreadViewController.swift
+//  Kiosk
+//
+//  Created by 기 표 on 1/17/24.
+//
+
+import UIKit
+
+class ChoiceBreadViewController: CommonViewController {
+    
+    let breadStore = BreadStore()
+    var choiceBreadCollectionView: UICollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
+    
+    var screenWidth = 0.0
+    var screenHeight = 0.0
+    
+    let choice15cmButton = ChoiceButton()
+    let choice30cmButton = ChoiceButton()
+    let choiceSelectedButton = ChoiceButton()
+    let choiceNotSelectedButton = ChoiceButton()
+    let breadButton = BreadButton()
+    
+    let choiceCmLabel = UILabel()
+    let choiceToastingLabel = UILabel()
+    let choiceBreadLabel = UILabel()
+    
+    override func setupView() {
+        super.setupView()
+        
+        screenWidth = self.view.bounds.width
+        screenHeight = self.view.bounds.height
+        
+        choiceBreadCollectionView = {
+            let collectionViewLayout = UICollectionViewFlowLayout()
+            collectionViewLayout.itemSize = CGSize(width: screenWidth / 5.0, height: screenHeight / 3.0)
+            collectionViewLayout.scrollDirection = .vertical
+            collectionViewLayout.minimumLineSpacing = 80
+            collectionViewLayout.minimumInteritemSpacing = 10
+            let collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewLayout)
+                collectionView.translatesAutoresizingMaskIntoConstraints = false
+                collectionView.register(BreadButton.self, forCellWithReuseIdentifier: breadButton.breadButtonID)
+                return collectionView
+            }()
+
+        
+        customNavigationBar.title = "빵 선택"
+        
+        choiceBreadCollectionView.backgroundColor = .systemGray6
+        
+        choiceCmLabel.text = "사이즈 선택"
+        choiceCmLabel.textColor = .black
+        choiceCmLabel.font = .systemFont(ofSize: 35, weight: .regular)
+        
+        choice15cmButton.buttonTitle = "15cm"
+        choice15cmButton.isSelected = true
+        
+        choice30cmButton.buttonTitle = "30cm"
+        choice30cmButton.isSelected = false
+        
+        choiceToastingLabel.text = "빵 토스팅"
+        choiceToastingLabel.textColor = .black
+        choiceToastingLabel.font = .systemFont(ofSize: 35, weight: .regular)
+        
+        choiceSelectedButton.buttonTitle = "선택"
+        choiceSelectedButton.isSelected = true
+        
+        choiceNotSelectedButton.buttonTitle = "미선택"
+        choiceNotSelectedButton.isSelected = false
+        
+        choiceBreadLabel.text = "빵 선택"
+        choiceBreadLabel.textColor = .black
+        choiceBreadLabel.font = .systemFont(ofSize: 35, weight: .regular)
+        
+        view.customAddSubView(choiceCmLabel)
+        view.customAddSubView(choiceToastingLabel)
+        view.customAddSubView(choiceBreadLabel)
+        view.customAddSubView(choiceSelectedButton)
+        view.customAddSubView(choiceNotSelectedButton)
+        view.customAddSubView(choice15cmButton)
+        view.customAddSubView(choice30cmButton)
+        view.customAddSubView(choiceBreadCollectionView)
+        
+        choiceBreadCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            // 사이즈 선택
+            choiceCmLabel.centerYAnchor.constraint(equalTo: safeArea.topAnchor, constant: 141),
+            choiceCmLabel.centerXAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 110),
+            
+            // 15cm
+            choice15cmButton.centerYAnchor.constraint(equalTo: safeArea.topAnchor, constant: 220),
+            choice15cmButton.centerXAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 200),
+            choice15cmButton.widthAnchor.constraint(equalToConstant: 343),
+            choice15cmButton.heightAnchor.constraint(equalToConstant: 83),
+            
+            // 30cm
+            choice30cmButton.centerYAnchor.constraint(equalTo: safeArea.topAnchor, constant: 220),
+            choice30cmButton.leadingAnchor.constraint(equalTo: choice15cmButton.trailingAnchor, constant: 25),
+            choice30cmButton.widthAnchor.constraint(equalToConstant: 343),
+            choice30cmButton.heightAnchor.constraint(equalToConstant: 83),
+            
+            // 빵 토스팅
+            choiceToastingLabel.topAnchor.constraint(equalTo: choice15cmButton.bottomAnchor, constant: 33),
+            choiceToastingLabel.centerXAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 98),
+            
+            // 선택
+            choiceSelectedButton.topAnchor.constraint(equalTo: choice15cmButton.bottomAnchor, constant: 90),
+            choiceSelectedButton.centerXAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 200),
+            choiceSelectedButton.widthAnchor.constraint(equalToConstant: 343),
+            choiceSelectedButton.heightAnchor.constraint(equalToConstant: 83),
+            
+            // 미선택
+            choiceNotSelectedButton.topAnchor.constraint(equalTo: choice30cmButton.bottomAnchor, constant: 90),
+            choiceNotSelectedButton.leadingAnchor.constraint(equalTo: choiceSelectedButton.trailingAnchor, constant: 25),
+            choiceNotSelectedButton.widthAnchor.constraint(equalToConstant: 343),
+            choiceNotSelectedButton.heightAnchor.constraint(equalToConstant: 83),
+            
+            // 빵 토스팅
+            choiceBreadLabel.topAnchor.constraint(equalTo: choiceSelectedButton.bottomAnchor, constant: 33),
+            choiceBreadLabel.centerXAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 81),
+            
+            // 빵
+            choiceBreadCollectionView.topAnchor.constraint(equalTo: choiceSelectedButton.bottomAnchor, constant: 90),
+            choiceBreadCollectionView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            choiceBreadCollectionView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            choiceBreadCollectionView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor)
+        ])
+        
+        choiceBreadCollectionView.delegate = self
+        choiceBreadCollectionView.dataSource = self
+        choiceBreadCollectionView.register(BreadButton.self, forCellWithReuseIdentifier: breadButton.breadButtonID)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
+extension ChoiceBreadViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return breadStore.bread.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell =             choiceBreadCollectionView.dequeueReusableCell(withReuseIdentifier: "BreadButton", for: indexPath) as! BreadButton
+        
+        cell.breadImageView.image = UIImage(named: "\(breadStore.bread[indexPath.row].breadEngTitle)")
+        cell.breadTitle = breadStore.bread[indexPath.row].breadTitle
+        cell.breadEngTitle = breadStore.bread[indexPath.row].breadEngTitle
+        cell.breadKcalTitle = String(breadStore.bread[indexPath.row].breadKcalTitle)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = choiceBreadCollectionView.cellForItem(at: indexPath) as! BreadButton
+//        cell.isSelect.toggle()
+        navigationController?.pushViewController(ToppingViewController(), animated: true)
+    }
+}
---
+++ Kiosk/Kiosk/View/SideBar.swift
@@ -0,0 +1,118 @@
+//
+//  SideBar.swift
+//  Kiosk
+//
+//  Created by 박상현 on 1/17/24.
+//
+
+import UIKit
+
+class SideBar: UIView {
+    var tableView: UITableView = UITableView()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        let containerView: UIView = {
+            let containerView = UIView()
+            containerView.backgroundColor = .init(named: "mainGreen")?.withAlphaComponent(0.65)
+            return containerView
+        }()
+        
+        // 사이드바 테이블뷰
+        tableView = {
+            let tableView = UITableView()
+            tableView.register(OrderListCell.self, forCellReuseIdentifier: OrderListCell.identifier)
+            tableView.rowHeight = UITableView.automaticDimension
+            tableView.separatorStyle = .none
+            tableView.backgroundColor = .clear
+            tableView.sectionHeaderTopPadding = .zero
+            tableView.delegate = self
+            tableView.dataSource = self
+            return tableView
+        }()
+        
+        customAddSubView(containerView)
+        customAddSubView(tableView)
+        
+        containerView.constraintEdge(self)
+        tableView.constraintEdge(containerView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension SideBar: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 7
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: OrderListCell.identifier, for: indexPath) as? OrderListCell else { return UITableViewCell() }
+        // 셀 선택 효과 제거
+        cell.selectionStyle = .none
+        
+        switch indexPath.row {
+        case 0:
+            cell.orderTitle = "사이즈"
+            cell.orderList = "15cm"
+        case 1:
+            cell.orderTitle = "빵종류"
+            cell.orderList = "플랫브레드(토스팅)"
+        case 2:
+            cell.orderTitle = "치즈"
+            cell.orderList = "슈레드 치즈"
+        case 3:
+            cell.orderTitle = "추가 토핑"
+            cell.orderList = "미트추가, 아보카도, 페퍼로니, 에그마요"
+        case 4:
+            cell.orderTitle = "야채"
+            cell.orderList = "양상추, 할라피뇨, 토마토, 올리브"
+        case 5:
+            cell.orderTitle = "소스"
+            cell.orderList = "스위트 어니언, 핫 칠리"
+        case 6:
+            cell.orderTitle = "세트 선택"
+            cell.orderList = "쿠키세트 (음료포함), 라즈베리 치즈케익"
+        default:
+            break
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView: UIView = {
+            let headerView = UIView()
+            headerView.backgroundColor = .init(named: "mainOrange")
+            return headerView
+        }()
+        
+        let productLabel: UILabel = {
+            let productLabel = UILabel()
+            productLabel.textColor = .black
+            productLabel.font = .systemFont(ofSize: 27, weight: .bold)
+            productLabel.text = "하프 랍스터 & 하프 쉬림프"
+            productLabel.textAlignment = .center
+            return productLabel
+        }()
+        
+        headerView.customAddSubView(productLabel)
+        
+        NSLayoutConstraint.activate([
+            productLabel.leadingAnchor.constraint(equalTo: headerView.leadingAnchor),
+            productLabel.trailingAnchor.constraint(equalTo: headerView.trailingAnchor),
+            productLabel.topAnchor.constraint(equalTo: headerView.topAnchor),
+            productLabel.bottomAnchor.constraint(equalTo: headerView.bottomAnchor),
+        ])
+        
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 65
+    }
+    
+}
