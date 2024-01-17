4fe86ddff402f2ef92f1469283fcb5aedf8eb85b
Sanghyeon Park <ios.humhae@gmail.com>
2024-01-17 15:39:02 +0900

    fix : conflict 복구

---
+++ Kiosk/Kiosk/View/OrderListCell.swift
@@ -0,0 +1,23 @@
+//
+//  OrderListCell.swift
+//  Kiosk
+//
+//  Created by 박상현 on 1/17/24.
+//
+
+import UIKit
+
+class OrderListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
