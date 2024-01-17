9b27c7a13586e78a98b437c7b4fa35ca1f4d5d99
JaeWoong Jeong <jjw8959@naver.com>
2024-01-17 18:13:10 +0900

    feat: 임시저장
    테스트

--- Kiosk/Kiosk/Controller/ToppingViewController.swift
+++ Kiosk/Kiosk/Controller/ToppingViewController.swift
@@ -10,16 +10,23 @@ import UIKit
 class ToppingViewController: CommonViewController {
     
     let cheeseStore = CheeseStore()
-    var cheeseCollectionView: UICollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
+    var cheeseCollectionView: UICollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    
+    var toppingCollectionView: UICollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    
     let cheeseButton = SandwichButton()
     
     var cheeseLabel = UILabel()
+    var toppingLabel = UILabel()
     
     var screenWidth = 0.0
     var screenHeight = 0.0
     
+    var cheeseCellHeight = 0.0
+    
     override func setupView() {
         super.setupView()
+        
         screenWidth = self.view.bounds.width
         screenHeight = self.view.bounds.height
         
@@ -27,7 +34,15 @@ class ToppingViewController: CommonViewController {
             let label = UILabel()
             label.text = "치즈 선택"
             label.textAlignment = .center
-            label.font = .systemFont(ofSize: 20)
+            label.font = .systemFont(ofSize: 40, weight: .heavy)
+            return label
+        }()
+        
+        toppingLabel = {
+            let label = UILabel()
+            label.text = "추가 토핑 선택"
+            label.textAlignment = .center
+            label.font = .systemFont(ofSize: 40, weight: .heavy)
             return label
         }()
         
@@ -37,17 +52,36 @@ class ToppingViewController: CommonViewController {
             collectionViewLayout.scrollDirection = .vertical
             collectionViewLayout.minimumLineSpacing = 80
             collectionViewLayout.minimumInteritemSpacing = 10
+            
             let collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewLayout)
             collectionView.translatesAutoresizingMaskIntoConstraints = false
             collectionView.register(SandwichButton.self, forCellWithReuseIdentifier: cheeseButton.sandwichButtonID)
+            cheeseCellHeight = collectionViewLayout.itemSize.height
             return collectionView
         }()
+        
+        toppingCollectionView = {
+            let collectionViewLayout = UICollectionViewFlowLayout()
+            collectionViewLayout.itemSize = CGSize(width: screenWidth / 5.0, height: screenHeight / 3.0)
+            collectionViewLayout.scrollDirection = .vertical
+            collectionViewLayout.minimumLineSpacing = 80
+            collectionViewLayout.minimumInteritemSpacing = 10
+            
+            let collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewLayout)
+            collectionView.translatesAutoresizingMaskIntoConstraints = false
+            collectionView.register(SandwichButton.self, forCellWithReuseIdentifier: cheeseButton.sandwichButtonID)
+            cheeseCellHeight = collectionViewLayout.itemSize.height
+            return collectionView
+        }()
+        
+        
         
         customNavigationBar.delegate = self
         customNavigationBar.title = "토핑 테스트"
         cheeseCollectionView.backgroundColor = .systemGray6
         self.view.customAddSubView(cheeseLabel)
         self.view.addSubview(cheeseCollectionView)
+        self.view.customAddSubView(toppingLabel)
         
         cheeseCollectionView.translatesAutoresizingMaskIntoConstraints = false
         
@@ -56,9 +90,14 @@ class ToppingViewController: CommonViewController {
             cheeseLabel.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 40),
             
             cheeseCollectionView.topAnchor.constraint(equalTo: cheeseLabel.bottomAnchor, constant: 20),
-            cheeseCollectionView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            cheeseCollectionView.heightAnchor.constraint(equalToConstant: cheeseCellHeight + 40),
             cheeseCollectionView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 40),
             cheeseCollectionView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -40),
+            
+            toppingLabel.topAnchor.constraint(equalTo: cheeseCollectionView.bottomAnchor, constant: 20),
+            toppingLabel.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 20)
+            
+            
             
         ])
         
@@ -79,6 +118,12 @@ extension ToppingViewController: UICollectionViewDelegate, UICollectionViewDataS
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        if collectionView == cheeseCollectionView {
+//            
+//        } else {
+//            
+//        }
+        
         let cell = cheeseCollectionView.dequeueReusableCell(withReuseIdentifier: "SandwichButton", for: indexPath) as! SandwichButton
         
         cell.sandwichImageView.image = UIImage(named: "card")
@@ -92,7 +137,13 @@ extension ToppingViewController: UICollectionViewDelegate, UICollectionViewDataS
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        var selectedCellIndex: IndexPath?
+        
         let cell = cheeseCollectionView.cellForItem(at: indexPath) as! SandwichButton
+        
+        if let selectedCellIndex = selectedCellIndex {
+            collectionView.deselectItem(at: selectedCellIndex, animated: false)
+        }
         cell.isSelect.toggle()
     }
     
